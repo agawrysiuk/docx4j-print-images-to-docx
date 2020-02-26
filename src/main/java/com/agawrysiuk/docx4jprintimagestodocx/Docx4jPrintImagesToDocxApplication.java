f78a9029d5ebf8a85d8ad0ce2affd117d9e882cd
dlrzz <arkad.gawry@gmail.com>
2020-02-26 21:09:24 +0100

    Added JavaFX dependencies, app started

--- src/main/java/com/agawrysiuk/docx4jprintimagestodocx/Docx4jPrintImagesToDocxApplication.java
+++ src/main/java/com/agawrysiuk/docx4jprintimagestodocx/Docx4jPrintImagesToDocxApplication.java
@@ -3,6 +3,8 @@ package com.agawrysiuk.docx4jprintimagestodocx;
 import com.agawrysiuk.docx4jprintimagestodocx.downloader.Downloader;
 import com.agawrysiuk.docx4jprintimagestodocx.printer.Docx4JPrinter;
 import com.agawrysiuk.docx4jprintimagestodocx.service.DeckLoader;
+import javafx.application.Application;
+import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 import org.docx4j.model.structure.PageSizePaper;
 
@@ -10,13 +12,14 @@ import java.io.IOException;
 import java.util.List;
 
 @Slf4j
-public class Docx4jPrintImagesToDocxApplication {
+public class Docx4jPrintImagesToDocxApplication extends Application {
 
-	public static void main(String[] args) throws IOException {
-		init();
+	public static void main(String[] args) {
+		launch(args);
 	}
 
-	private static void init() throws IOException {
+	@Override
+	public void start(Stage stage) throws Exception {
 		DeckLoader deckLoader = new DeckLoader(new Downloader());
 		List<String> arrayLink = deckLoader.loadDeck();
 		Docx4JPrinter printer = Docx4JPrinter.builder()
@@ -32,6 +35,7 @@ public class Docx4jPrintImagesToDocxApplication {
 			e.printStackTrace();
 			log.warn("Can't print the picture!");
 		}
+		System.exit(0);
 	}
 
 }
