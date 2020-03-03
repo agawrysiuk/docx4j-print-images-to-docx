b8555825921d03001ba8dbc9a401fea0e7632522
dlrzz <arkad.gawry@gmail.com>
2020-03-03 19:35:11 +0100

    Downloader refactoring

--- src/main/java/com/agawrysiuk/docx4jprintimagestodocx/downloader/Downloader.java
+++ src/main/java/com/agawrysiuk/docx4jprintimagestodocx/downloader/Downloader.java
@@ -18,34 +18,62 @@ public class Downloader {
         return cardList;
     }
 
-    public Card downloadCard(String cardTitle){
+    public Card downloadCard(String cardTitle) {
 
-        cardTitle = cardTitle.replace(" ","+"); //change to string
+        String searchLink = prepareCardSearchLink(cardTitle);
+        JSONObject downloadedCard = getJsonFromScryfall(searchLink);
+        if (downloadedCard != null) {
+            return createCardObject(downloadedCard);
+        }
+        return null;
+    }
+
+    private String prepareCardSearchLink(String cardTitle) {
+        StringBuilder link = new StringBuilder();
+        link.append("https://api.scryfall.com/cards/named?exact=");
+        cardTitle = cardTitle.replace(" ", "+");
+        link.append(cardTitle);
+        return link.toString();
+    }
+
+    private JSONObject getJsonFromScryfall(String scryfallLink) {
         Document scryfallDocument;
         try {
-            scryfallDocument = Jsoup.connect("https://api.scryfall.com/cards/named?fuzzy="+cardTitle).ignoreContentType(true).get();
+            scryfallDocument = Jsoup.connect(scryfallLink).ignoreContentType(true).get();
         } catch (IOException e) {
             System.out.println(e.getMessage());
             return null;
         }
 
-        JSONObject downloadedCard = new JSONObject(scryfallDocument.body().text());
+        return new JSONObject(scryfallDocument.body().text());
+    }
 
-        // TODO: 2020-02-25 add some serializer
+
+    private Card createCardObject(JSONObject downloadedCard){
         Card card = new Card();
-        if(downloadedCard.has("card_faces") && !downloadedCard.has("image_uris")) {
-            card.setCardImageLargeLink(downloadedCard.getJSONArray("card_faces").getJSONObject(0).getJSONObject("image_uris").getString("large"));
-            card.setCardImage(new Image(downloadedCard.getJSONArray("card_faces").getJSONObject(0).getJSONObject("image_uris").getString("large")));
-            card.setCardImageTransformLargeLink(downloadedCard.getJSONArray("card_faces").getJSONObject(1).getJSONObject("image_uris").getString("large"));
-            card.setCardImageTransform(new Image(downloadedCard.getJSONArray("card_faces").getJSONObject(1).getJSONObject("image_uris").getString("large")));
+        if (isCardTwoFaced(downloadedCard)) {
+            setTwoCardImages(card,downloadedCard);
         } else {
-            card.setCardImageLargeLink(downloadedCard.getJSONObject("image_uris").getString("large"));
-            card.setCardImage(new Image(downloadedCard.getJSONObject("image_uris").getString("large")));
-            card.setCardImageTransformLargeLink(null);
+            setOneCardImage(card,downloadedCard);
         }
-
         card.setName(downloadedCard.getString("name"));
-
         return card;
+    }
+
+    private boolean isCardTwoFaced(JSONObject downloadedCard) {
+        return !downloadedCard.has("image_uris") && downloadedCard.has("card_faces");
+    }
+
+    private void setTwoCardImages(Card card, JSONObject downloadedCard) {
+        card.setCardImageLargeLink(downloadedCard.getJSONArray("card_faces").getJSONObject(0).getJSONObject("image_uris").getString("large"));
+        card.setCardImage(new Image(downloadedCard.getJSONArray("card_faces").getJSONObject(0).getJSONObject("image_uris").getString("large")));
+        card.setCardImageTransformLargeLink(downloadedCard.getJSONArray("card_faces").getJSONObject(1).getJSONObject("image_uris").getString("large"));
+        card.setCardImageTransform(new Image(downloadedCard.getJSONArray("card_faces").getJSONObject(1).getJSONObject("image_uris").getString("large")));
+    }
+
+    private void setOneCardImage(Card card, JSONObject downloadedCard) {
+        card.setCardImageLargeLink(downloadedCard.getJSONObject("image_uris").getString("large"));
+        card.setCardImage(new Image(downloadedCard.getJSONObject("image_uris").getString("large")));
+        card.setCardImageTransformLargeLink(null);
     }
 }
