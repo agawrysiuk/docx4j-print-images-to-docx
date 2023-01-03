1998031b09d21fe9c3e5ed487a2b9fa609579018
agawrysiuk <arkad.gawry@gmail.com>
2023-01-03 20:18:29 +0100

    Created dummy frontend application

--- new-frontend/src/app/app.component.ts
+++ new-frontend/src/app/app.component.ts
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'new-frontend';
+  cards: Card[] = []
+
+  downloadPaths() {
+    this.cards = [
+      {name: "asd", path: "https://cards.scryfall.io/large/front/9/1/91d9bb89-d8f8-4dff-8b94-3f7b8aa8f299.jpg?1593274717"},
+      {name: "asd", path: "https://cards.scryfall.io/large/front/9/1/91d9bb89-d8f8-4dff-8b94-3f7b8aa8f299.jpg?1593274717"},
+      {name: "asd", path: "https://cards.scryfall.io/large/front/9/1/91d9bb89-d8f8-4dff-8b94-3f7b8aa8f299.jpg?1593274717"},
+      {name: "asd", path: "https://cards.scryfall.io/large/front/9/1/91d9bb89-d8f8-4dff-8b94-3f7b8aa8f299.jpg?1593274717"},
+      {name: "asd", path: "https://cards.scryfall.io/large/front/9/1/91d9bb89-d8f8-4dff-8b94-3f7b8aa8f299.jpg?1593274717"},
+      {name: "asd", path: "https://cards.scryfall.io/large/front/9/1/91d9bb89-d8f8-4dff-8b94-3f7b8aa8f299.jpg?1593274717"},
+      {name: "asd", path: "https://cards.scryfall.io/large/front/9/1/91d9bb89-d8f8-4dff-8b94-3f7b8aa8f299.jpg?1593274717"},
+      {name: "asd", path: "https://cards.scryfall.io/large/front/9/1/91d9bb89-d8f8-4dff-8b94-3f7b8aa8f299.jpg?1593274717"},
+      {name: "asd", path: "https://cards.scryfall.io/large/front/9/1/91d9bb89-d8f8-4dff-8b94-3f7b8aa8f299.jpg?1593274717"},
+      {name: "asd", path: "https://cards.scryfall.io/large/front/9/1/91d9bb89-d8f8-4dff-8b94-3f7b8aa8f299.jpg?1593274717"},
+      {name: "asd", path: "https://cards.scryfall.io/large/front/9/1/91d9bb89-d8f8-4dff-8b94-3f7b8aa8f299.jpg?1593274717"},
+      {name: "asd", path: "https://cards.scryfall.io/large/front/9/1/91d9bb89-d8f8-4dff-8b94-3f7b8aa8f299.jpg?1593274717"},
+      {name: "asd", path: "https://cards.scryfall.io/large/front/9/1/91d9bb89-d8f8-4dff-8b94-3f7b8aa8f299.jpg?1593274717"},
+      {name: "asd", path: "https://cards.scryfall.io/large/front/9/1/91d9bb89-d8f8-4dff-8b94-3f7b8aa8f299.jpg?1593274717"},
+      {name: "asd", path: "https://cards.scryfall.io/large/front/9/1/91d9bb89-d8f8-4dff-8b94-3f7b8aa8f299.jpg?1593274717"},
+      {name: "asd", path: "https://cards.scryfall.io/large/front/9/1/91d9bb89-d8f8-4dff-8b94-3f7b8aa8f299.jpg?1593274717"},
+      {name: "asd", path: "https://cards.scryfall.io/large/front/9/1/91d9bb89-d8f8-4dff-8b94-3f7b8aa8f299.jpg?1593274717"},
+      {name: "asd", path: "https://cards.scryfall.io/large/front/9/1/91d9bb89-d8f8-4dff-8b94-3f7b8aa8f299.jpg?1593274717"},
+      {name: "asd", path: "https://cards.scryfall.io/large/front/9/1/91d9bb89-d8f8-4dff-8b94-3f7b8aa8f299.jpg?1593274717"},
+    ]
+  }
+
+  clean() {
+    this.cards = []
+  }
+}
+
+type Card = {
+  name: string;
+  path: string;
 }
