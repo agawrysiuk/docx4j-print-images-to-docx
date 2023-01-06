d484bfa43eace6a16a16c3cc424ab11b31dc5f32
agawrysiuk <arkad.gawry@gmail.com>
2023-01-06 20:02:35 +0100

    Changing card getter to have list of name cards

--- new-mtg-downloader/src/test/java/com/agawrysiuk/newmtgdownloader/downloader/DownloaderControllerTest.java
+++ new-mtg-downloader/src/test/java/com/agawrysiuk/newmtgdownloader/downloader/DownloaderControllerTest.java
@@ -1,6 +1,5 @@
 package com.agawrysiuk.newmtgdownloader.downloader;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,10 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 
 import java.util.List;
-import java.util.Map;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
