c7ef48aa0a5643bde8e8a1def967e40828cf69dd
agawrysiuk <arkad.gawry@gmail.com>
2023-01-06 20:21:07 +0100

    Added CORS exception

--- new-mtg-downloader/src/main/java/com/agawrysiuk/newmtgdownloader/downloader/DownloaderController.java
+++ new-mtg-downloader/src/main/java/com/agawrysiuk/newmtgdownloader/downloader/DownloaderController.java
@@ -2,6 +2,7 @@ package com.agawrysiuk.newmtgdownloader.downloader;
 
 import com.agawrysiuk.newmtgdownloader.data.Card;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 @RequiredArgsConstructor
 public class DownloaderController {
 
