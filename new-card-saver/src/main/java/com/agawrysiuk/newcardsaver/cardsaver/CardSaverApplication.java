3958300fe1a35cf352dcc4ba7ae051b9d3f6ca0a
agawrysiuk <arkad.gawry@gmail.com>
2023-01-06 20:28:04 +0100

    Added empty application to save cards

---
+++ new-card-saver/src/main/java/com/agawrysiuk/newcardsaver/cardsaver/CardSaverApplication.java
@@ -0,0 +1,13 @@
+package com.agawrysiuk.newcardsaver.cardsaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardSaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardSaverApplication.class, args);
+	}
+
+}
