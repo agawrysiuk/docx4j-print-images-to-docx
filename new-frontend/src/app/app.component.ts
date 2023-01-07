641eef072dd3bec6f33511d72ba02f8ebf4d3d57
agawrysiuk <arkad.gawry@gmail.com>
2023-01-07 21:46:16 +0100

    Fixing downloading and printing multifaces cards on the frontend

--- new-frontend/src/app/app.component.ts
+++ new-frontend/src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
   title = 'new-frontend';
   cards: Card[] = [];
   cardNames: string = "";
+  cardLinks: string[] = [];
 
   constructor(private appHttpService: AppHttpService) {}
 
@@ -18,12 +19,32 @@ export class AppComponent {
     const splitArray = this.cardNames.split(/\r?\n/).filter(element => element);
     this.appHttpService.getCards(splitArray).then((res: Card[]) => {
       this.cards = res
+      const regularLinks = res.filter(card => card.image_uris).map(card => card.image_uris!!.large)
+      const faceLinks = res.filter(card => card.card_faces).flatMap(card => card.card_faces).map(face => face!!.image_uris.large)
+      this.cardLinks = regularLinks.concat(faceLinks)
     })
   }
 
   print() {
-    this.appHttpService.printCards(this.cards.map(card => card.image_uris.large).filter(link => link)).then(res => {
+    this.appHttpService.printCards(this.cardLinks).then(res => {
       console.log(res);
     })
   }
 }
+
+/* 
+TEST LIST:
+
+Meren of Clan Nel Toth
+Viscera Seer
+Shambling Ghast
+Ragavan Nimble Pilferer
+Prosperous Innkeeper
+Mahadi Emporium Master
+Jolene the Plunder Queen
+Tendershoot Dryad
+Scute Swarm
+Dockside Extortionist
+Tymaret the Murder King
+Fable of the Mirror-Breaker
+*/
--- new-frontend/src/app/card.ts
+++ new-frontend/src/app/card.ts
@@ -2,7 +2,14 @@ export interface Card {
     id: string;
     uri: string;
     name: string;
-    image_uris: {
-        large: string;
-    };
+    image_uris?: ImageUris;
+    card_faces?: CardFaces[];
+}
+
+interface ImageUris {
+    large: string;
+}
+
+interface CardFaces {
+    image_uris: ImageUris;
 }
