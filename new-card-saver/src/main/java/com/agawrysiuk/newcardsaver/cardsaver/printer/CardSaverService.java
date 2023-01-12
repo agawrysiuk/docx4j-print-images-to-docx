26b5b8fcd460aedc0ed1d367d2f31ab31ba858fd
agawrysiuk <arkad.gawry@gmail.com>
2023-01-12 17:13:17 +0100

    Saving info.txt

--- new-card-saver/src/main/java/com/agawrysiuk/newcardsaver/cardsaver/printer/CardSaverService.java
+++ new-card-saver/src/main/java/com/agawrysiuk/newcardsaver/cardsaver/printer/CardSaverService.java
@@ -1,7 +1,6 @@
 package com.agawrysiuk.newcardsaver.cardsaver.printer;
 
 import com.agawrysiuk.newcardsaver.cardsaver.common.ApplicationEnvVariables;
-import com.agawrysiuk.newcardsaver.cardsaver.common.StringExtractor;
 import com.agawrysiuk.newcardsaver.cardsaver.dto.SaveToFolderRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
@@ -17,6 +16,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static com.agawrysiuk.newcardsaver.cardsaver.common.StringExtractor.parseFileName;
+import static com.agawrysiuk.newcardsaver.cardsaver.common.StringExtractor.removeParams;
+import static java.lang.System.lineSeparator;
+import static java.util.stream.Collectors.counting;
 
 @Service
 @Slf4j
@@ -40,9 +46,19 @@ public class CardSaverService {
         printer.print();
     }
 
+    @SneakyThrows
     public void toFolder(SaveToFolderRequest request) {
-        request.getCardLinks().forEach(link -> downloadToFolder(link, request.getFolderPath()));
+        Map<String, Long> counts = request.getCardLinks().stream()
+                .collect(Collectors.groupingBy(e -> e, counting()));
+        String parentFolder = envVariables.getParentSaveFolder();
+        Path folderPath = Files.createDirectories(Paths.get(parentFolder).resolve(request.getFolderPath()));
+
+        counts.keySet().forEach(link -> downloadToFolder(link, folderPath));
         log.info("All images saved.");
+        String infoTxtContent = counts.entrySet().stream().map(entry -> "" + entry.getValue() + "," + parseFileName(entry.getKey()))
+                .collect(Collectors.joining(lineSeparator()));
+        Files.writeString(folderPath.resolve("info.txt"), infoTxtContent);
+        log.info("info.txt saved.");
     }
 
     @SneakyThrows
@@ -57,11 +73,9 @@ public class CardSaverService {
     }
 
     @SneakyThrows
-    public void downloadToFolder(String link, String folderName) {
-        try(InputStream in = new URL(StringExtractor.removeParams(link)).openStream()){
-            String parentFolder = envVariables.getParentSaveFolder();
-            Path folderPath = Files.createDirectories(Paths.get(parentFolder).resolve(folderName));
-            Path fullPath = folderPath.resolve(StringExtractor.parseFileName(link));
+    public void downloadToFolder(String link, Path folderPath) {
+        try (InputStream in = new URL(removeParams(link)).openStream()) {
+            Path fullPath = folderPath.resolve(parseFileName(link));
             Files.copy(in, fullPath);
             log.info("Saved {} to {}", link, fullPath);
         }
