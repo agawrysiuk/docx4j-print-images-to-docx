d0bea23bff2a2def833ae75ca826d03c9df6b8e9
agawrysiuk <arkad.gawry@gmail.com>
2023-01-23 19:09:19 +0100

    Moved some things to Local 2

--- new-card-saver/src/main/java/com/agawrysiuk/newcardsaver/cardsaver/printer/CardSaverService.java
+++ new-card-saver/src/main/java/com/agawrysiuk/newcardsaver/cardsaver/printer/CardSaverService.java
@@ -2,8 +2,6 @@ package com.agawrysiuk.newcardsaver.cardsaver.printer;
 
 import com.agawrysiuk.newcardsaver.cardsaver.common.ApplicationEnvVariables;
 import com.agawrysiuk.newcardsaver.cardsaver.dto.SaveToFolderRequest;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -20,8 +18,6 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 import static com.agawrysiuk.newcardsaver.cardsaver.common.StringExtractor.parseFileName;
 import static com.agawrysiuk.newcardsaver.cardsaver.common.StringExtractor.removeParams;
@@ -85,60 +81,5 @@ public class CardSaverService {
             Files.copy(in, fullPath);
             log.info("Saved {} to {}", link, fullPath);
         }
-    }
-
-    @SneakyThrows
-    public void fromFolderToDocxByFolderContent(String folderPath) {
-        try (Stream<Path> paths = Files.walk(Paths.get(folderPath))) {
-            List<byte[]> byteList = paths
-                    .filter(Files::isRegularFile)
-                    .map(this::toBytes)
-                    .toList();
-
-            Docx4JPrinter printer = Docx4JPrinter.builder()
-                    .pictureLinks(byteList)
-                    .fileName("test-print.docx")
-                    .landscape(true)
-                    .maxWidth(3600) //3580
-                    .pageSize(PageSizePaper.LETTER)
-                    .build();
-
-            printer.print();
-        }
-    }
-
-    @SneakyThrows
-    public void fromFolderToDocxByTxtContent(String infoTxtPath, String folderPath) {
-        List<byte[]> byteList = Files.readAllLines(Path.of(infoTxtPath)).stream()
-                .filter(line -> !line.isEmpty())
-                .map(line -> {
-                    var lineInfo = line.split(",");
-                    return new ContentTxtLines(Integer.parseInt(lineInfo[0]), lineInfo[1]);
-                })
-                .flatMap(contentTxtLines ->
-                        IntStream.range(0, contentTxtLines.numOfCards).mapToObj(n -> toBytes(Path.of(folderPath).resolve(contentTxtLines.cardFilename))))
-                .toList();
-
-        Docx4JPrinter printer = Docx4JPrinter.builder()
-                .pictureLinks(byteList)
-                .fileName("test-print.docx")
-                .landscape(true)
-                .maxWidth(3600) //3580
-                .pageSize(PageSizePaper.LETTER)
-                .build();
-
-        printer.print();
-    }
-
-    @SneakyThrows
-    private byte[] toBytes(Path path) {
-        return Files.readAllBytes(path);
-    }
-
-    @AllArgsConstructor
-    @Getter
-    private static class ContentTxtLines {
-        private int numOfCards;
-        private String cardFilename;
     }
 }
